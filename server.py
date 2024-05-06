83055872de3eb7dc74bd79faf7de6d1425771095
= <esyeddanishabbas@gmail.com>
2024-05-06 11:45:03 +0200

    completed

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 import json
-
+from waitress import serve
 app = Flask(__name__)
 
 # Function to load JSON data from file
@@ -9,17 +9,32 @@ def load_json(filename):
         data = json.load(f)
     return data
 
-# Route to serve test1.json
-@app.route('/test1.json', methods=['GET'])
+# Route to serve eng 1000.json
+@app.route('/emg1000.jsonld', methods=['GET'])
 def get_test1():
-    data = load_json('test1.json')
+    data = load_json('emg1000.jsonld')
     return jsonify(data)
 
-# Route to serve test2.json
-@app.route('/test2.json', methods=['GET'])
+# Route to serve emgfeatures.json
+@app.route('/emgFeatures.jsonld', methods=['GET'])
 def get_test2():
-    data = load_json('test2.json')
+    data = load_json('emgFeatures.jsonld')
+    return jsonify(data)
+
+@app.route('/polarACC.jsonld', methods=['GET'])
+def get_test3():
+    data = load_json('polarACC.jsonld')
+    return jsonify(data)
+
+@app.route('/polarECG.jsonld', methods=['GET'])
+def get_test4():
+    data = load_json('polarECG.jsonld')
+    return jsonify(data)
+
+@app.route('/polarHR.jsonld', methods=['GET'])
+def get_test5():
+    data = load_json('polarHR.jsonld')
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    serve(app, host= "0.0.0.0", port= 5000)
